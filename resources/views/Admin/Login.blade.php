7b37ba2b980ebbfd73a88e2074f91e2ea8adf88f
renpenghai <1376148965@qq.com>
2018-11-30 18:06:45 +0800

    登录提交1.0.1

--- resources/views/Admin/Login.blade.php
+++ resources/views/Admin/Login.blade.php
@@ -1,54 +1,53 @@
+
 <!DOCTYPE html>
-<html>
+<html lang="en" class="no-js">
+
 <head>
+
   <meta charset="utf-8">
-  <title>登录--OpenAuth.Net</title>
-  <meta name="renderer" content="webkit">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-  <meta name="apple-mobile-web-app-status-bar-style" content="black">
-  <meta name="apple-mobile-web-app-capable" content="yes">
-  <meta name="format-detection" content="telephone=no">
+  <title>Fullscreen Login</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author" content="">
 
-  <link rel="stylesheet" href="{{asset('resources/css/login.css')}}" media="all" />
+  <!-- CSS -->
+  <link rel='stylesheet' href='http://fonts.googleapis.com/css?family=PT+Sans:400,700'>
+  <link rel="stylesheet" href="{{asset('resources/css/Login/reset.css')}}">
+  <link rel="stylesheet" href="{{asset('resources/css/Login/supersized.css')}}">
+  <link rel="stylesheet" href="{{asset('resources/css/Login/style.css')}}">
+
+  <!-- Javascript -->
+  <script src="{{asset('resources/js/Login/jquery-1.8.2.min.js')}}"></script>
+  <script src="{{asset('resources/js/Login/supersized.3.2.7.min.js')}}"></script>
+  <script src="{{asset('resources/js/Login/supersized-init.js')}}"></script>
+  <script src="{{asset('resources/js/Login/scripts.js')}}"></script>
+
+  <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+  <!--[if lt IE 9]>
+  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+
 </head>
-<body class="main" style="background-position: -280.8px 0px;">
 
-<div id="mainBody">
-  <div id="cloud1" class="cloud" style="background-position: 891.1px 100px;"></div>
-  <div id="cloud2" class="cloud" style="background-position: 401px 460px;"></div>
-</div>
-<div class="loginbody">
+<body>
 
-  <span class="systemlogo"></span>
+<div class="page-container">
+  <h1>博客后台管理</h1>
+  <form action="" method="post">
+    <input type="text" name="username" class="username" placeholder="账号">
+    <input type="password" name="password" class="password" placeholder="密码">
+    <div style="white-space: nowrap;float:left;width: 305px;">
+      <div style="float: left"><input type="text" name="password" class="verification" placeholder="验证码"></div>
 
-  <div class="loginbox" style="position: absolute; left: 294px;">
-    <div style="width:165px; height:96px; position:absolute;top:8px;left:-70px">
-      <div class="tou"></div>
-      <div class="initial_left_hand" id="left_hand"></div>
-      <div class="initial_right_hand" id="right_hand"></div>
+      <div class="verification"></div>
     </div>
-    <form class="layui-form">
-      <ul>
-        <li>
-          <input name="username" type="text" class="loginuser" value="System"
-                 placeholder="请输入用户名/手机号码" lay-verify="required">
-        </li>
-        <li>
-          <input name="password" type="password" id="password" value="123456"
-                 class="loginpwd ipt" placeholder="请输入密码" lay-verify="required">
-        </li>
-        <li>
-          <input name="username" type="text" class="loginuser" value="System"
-                 placeholder="请输入用户名/手机号码" lay-verify="required" width="200px">
-          <button class="layui-btn loginbtn" lay-submit  lay-filter="login">登录</button>
-        </li>
-      </ul>
-    </form>
+    <button type="submit">Sign me in</button>
+    <div class="error"><span>+</span></div>
+  </form>
+  <div class="connect">
   </div>
-
 </div>
-
-<div class="loginbm">版权所有 2018 </div>
 </body>
+
 </html>
+
