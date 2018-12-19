5e26569d69a727394f9635cfe1cd026d6387600a
renpenghai <1376148965@qq.com>
2018-12-19 12:20:22 +0800

    index1.0.1

--- resources/views/Admin/index.php
+++ resources/views/Admin/index.php
@@ -41,12 +41,13 @@
             </div>
             <h3>General</h3>
           </div>
+              <div class="clearfix"></div>
 
             <!-- /menu profile quick info -->
 
             <!-- sidebar menu -->
             <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
-              <div class="menu_section" style="overflow:auto;height: 500px;">
+              <div class="menu_section">
                 
                 <ul class="nav side-menu">
                   <li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
